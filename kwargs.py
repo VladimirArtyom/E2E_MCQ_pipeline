f5440fb0c01d09cedd3f04dff1bd0aba6b027240
vlad <mlprimitif@gmail.com>
2024-10-23 13:44:09 +0700

    Termine kwargs pour tout

--- kwargs.py
+++ kwargs.py
@@ -1,44 +1,44 @@
 from typing import Mapping, Any
 QAG_KWARGS: Mapping[str, Any] = {
-    "num_beams": 15,
-    "top_p": 0.98,
+    "num_beams": 10,
+    "top_p": 0.95,
     "top_k": 200,
-    "temperature": 0.8,
-    "max_length": 64,
-    "num_return_sequences": 10,
-    "repetition_penalty": 2.5,
+    "temperature": 2.0,
+    "max_length": 512,
+    "num_return_sequences": 1,
+    "repetition_penalty": 4.5,
     "early_stopping":True,
 }
 
 QG_KWARGS: Mapping[str, Any] = {
-    "num_beams": 15,
-    "top_p": 0.98,
+    "num_beams": 10,
+    "top_p": 0.95,
     "top_k": 200,
-    "temperature": 0.8,
+    "temperature": 1.0,
     "max_length": 64,
-    "num_return_sequences": 10,
-    "repetition_penalty": 2.5,
+    "num_return_sequences": 1,
+    "repetition_penalty": 3.5,
     "early_stopping":True,
 }
 
 DG_1_KWARGS: Mapping[str, Any] = {
-    "num_beams": 15,
+    "num_beams": 10,
     "top_p": 0.98,
     "top_k": 200,
     "temperature": 0.8,
     "max_length": 64,
-    "num_return_sequences": 10,
+    "num_return_sequences": 1,
     "repetition_penalty": 4.5,
     "early_stopping":True,
 }
 
 DG_ALL_KWARGS: Mapping[str, Any] = {
 
-    "num_beams": 15,
-    "top_p": 0.90,
-    "top_k": 200,
-    "repetition_penalty":8.5,
-    "temperature": 1.8,
+    "num_beams": 10,
+    "top_p": 0.8,
+    "top_k": 250,
+    "repetition_penalty":3.5,
+    "temperature": 2.5,
     "max_length": 512,
     "num_return_sequences": 1,
     "early_stopping":True,
