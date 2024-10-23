1894fb827154ededd2daf55968f37d36d4c02d07
vlad <mlprimitif@gmail.com>
2024-10-23 13:44:25 +0700

    Fix bug

--- distractor_generator.py
+++ distractor_generator.py
@@ -59,36 +59,3 @@ class DistractorGenerator(GenerationPipeline):
             return_tensors="pt"
         )
         return encoded["input_ids"], encoded["attention_mask"]
-"""
-
-if __name__ == "__main__":
-    qg_model = T5ForConditionalGeneration.from_pretrained("t5-small")
-    qg_tokenizer = T5Tokenizer.from_pretrained("t5-small")
-
-    qg_tokenizer.add_special_tokens({
-        "additional_special_tokens": [
-                                      E2E_TOKEN.QUESTION_TOKEN.value,
-                                      E2E_TOKEN.ANSWER_TOKEN.value,
-                                      E2E_TOKEN.CONTEXT_TOKEN.value,
-                                      E2E_TOKEN.SEP_TOKEN.value
-                                      ]
-    })
-
-    dg = DistractorGenerator(
-        qg_model,
-        qg_tokenizer,
-        max_length=512,
-        question_token=E2E_TOKEN.QUESTION_TOKEN,
-        context_token=E2E_TOKEN.CONTEXT_TOKEN,
-        answer_token=E2E_TOKEN.ANSWER_TOKEN,
-        sep_token=E2E_TOKEN.SEP_TOKEN,
-        device="cpu"
-    )
-    
-    answer = "Tripping"
-    question = "What is what is Bro ?"
-    context = "In the heart of a bustling city, a small park serves as a serene escape for residents and visitors alike. Towering trees provide shade on warm summer days, while vibrant flowers bloom in an array of colors, creating a picturesque landscape. Families gather for picnics on the grassy lawns, children laugh as they chase one another around the playground, and joggers find solace on the winding paths that meander through the greenery. The gentle sound of birds chirping and leaves rustling in the breeze adds to the tranquility, making it a perfect spot for reflection and relaxation. As the sun sets, the park transforms, with soft lights illuminating the pathways, inviting couples to stroll hand in hand under the twinkling stars. It is a cherished haven in the midst of urban chaos, reminding everyone of the beauty of nature and the importance of taking a moment to unwind."
-
-    dg(answer=answer, question=question, context=context)
-    
-"""
